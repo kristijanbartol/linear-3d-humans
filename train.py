48b505e20d09cd1b727615010ba4c1021f0203a4
kristijanbartol <kristijan.bartol@gmail.com>
2021-10-06 12:39:56 +0200

    Implement other machine learning models.

--- train.py
+++ train.py
@@ -1,19 +1,52 @@
 import argparse
 import numpy as np
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 
 from prepare import prepare
 
 
+class Models():
+
+    RANDOM_STATE = 37
+
+    @staticmethod
+    def linear():
+        return LinearRegression()
+
+    @staticmethod
+    def poly():
+        return make_pipeline(PolynomialFeatures(degree=5), LinearRegression())
+
+    @staticmethod
+    def tree():
+        return DecisionTreeRegressor(random_state=Models.RANDOM_STATE)
+
+    @staticmethod
+    def mlp():
+        return MLPRegressor(hidden_layer_sizes=(2000), random_state=Models.RANDOM_STATE, max_iter=500)
+
+
 def init_argparse():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--data_root', type=str, help='root data folder')
+        '--data_root', type=str, 
+        help='root data folder')
     parser.add_argument(
-        '--dataset_name', type=str, help='dataset name')
+        '--dataset_name', type=str, 
+        help='dataset name')
     parser.add_argument(
-        '--regressor_type', type=str, help='set of input features'
+        '--regressor_type', type=str,
+        help='regressor type defined by the set of input features'
+    )
+    parser.add_argument(
+        '--model', type=str, choices=['linear', 'poly', 'tree', 'mlp'],
+        help='machine learning model type'
     )
 
     return parser
@@ -27,10 +60,10 @@ if __name__ == '__main__':
     X, y, measurements = prepare(args)
     print('Train/test splitting...')
     #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    X_train, X_test, y_train, y_test = train_test_split(X, measurements, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
     print('Creating model...')
-    model = LinearRegression()
+    model = getattr(Models, args.model)()
     print('Fitting model...')
     reg = model.fit(X_train, y_train)
     print('Predicting...')
